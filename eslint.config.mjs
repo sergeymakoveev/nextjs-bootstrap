5c326a7ad8959e6304967e3297c5789b52c3eb8a
Sergey Makoveev <sergey@makoveev.info>
2025-10-23 14:39:10 +0300

    eslint: add flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,241 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// @ts-check
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+// import jestPlugin from 'eslint-plugin-jest';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
+import nextPlugin from '@next/eslint-plugin-next';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
+// import anyParser from 'any-eslint-parser';
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default defineConfig(
   {
+    // config with just ignores is the replacement for `.eslintignore`
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
   },
-];
-
-export default eslintConfig;
+  /*
+  {
+    files: ['*.svg'],
+    parser: anyParser,
+    parserOptions: {},
+    rules: {
+      'regex/invalid': [
+        'error',
+        [
+          {
+            message: 'Don\'t hardcode "width" in svg',
+            regex: '[ \\n]width=[\\\'|\\"](.*)?[\\\'|\\"]',
+          },
+          {
+            message: 'Don\'t hardcode "height" in svg',
+            regex: '[ \\n]height=[\\\'|\\"](.*)?[\\\'|\\"]',
+          },
+        ],
+      ],
+    },
+  },
+  */
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx}'],
+    extends: [eslint.configs.recommended, importPlugin.flatConfigs.recommended],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      // jest: jestPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          // project: ['./tsconfig.json'],
+        },
+      },
+      'import/extensions': [
+        '.js',
+        '.jsx',
+        '.ts',
+        '.tsx',
+        '.css',
+        '.scss',
+        '.svg',
+        '.json',
+      ],
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: ['typeLike'],
+          format: ['PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'enum',
+          format: ['UPPER_CASE', 'PascalCase'],
+        },
+        {
+          selector: 'parameter',
+          format: ['camelCase', 'PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'variable',
+          types: ['boolean'],
+          format: ['PascalCase'],
+          prefix: ['is', 'should', 'has', 'can', 'did', 'will', 'are'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'variable',
+          modifiers: ['const'],
+          types: ['function'],
+          format: ['camelCase', 'PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'variable',
+          modifiers: ['const'],
+          format: ['camelCase', 'PascalCase'],
+        },
+        {
+          selector: 'function',
+          format: ['camelCase', 'PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'variableLike',
+          format: ['camelCase', 'UPPER_CASE'],
+          leadingUnderscore: 'allow',
+        },
+      ],
+      'import/no-named-as-default-member': 'off',
+      'import/no-named-as-default': 'off',
+      // 'import/no-default-export': 'error',
+      'import/no-unresolved': ['error', { ignore: ['\\.svg\\?react$'] }],
+      'import/no-unused-modules': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-absolute-path': 'error',
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'unknown',
+          ],
+          'newlines-between': 'always',
+          pathGroups: [
+            {
+              pattern: '*.svg',
+              patternOptions: {
+                matchBase: true,
+              },
+              group: 'object',
+              position: 'before',
+            },
+            {
+              pattern: '*.{css,scss}',
+              patternOptions: {
+                matchBase: true,
+              },
+              group: 'object',
+              position: 'after',
+            },
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'parent',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: [],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.{jsx,tsx,mjsx}'],
+    extends: [
+      reactPlugin.configs.flat.recommended,
+      reactHooksPlugin.configs.flat.recommended,
+      importPlugin.flatConfigs.react,
+      // @ts-ignore
+      nextPlugin.flatConfig.recommended,
+      // @ts-ignore
+      nextPlugin.flatConfig.coreWebVitals,
+    ],
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      tseslint.configs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-redeclare': 'error',
+    },
+  },
+  {
+    // disable type-aware linting on JS files
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx}'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+  // {
+  //   // enable jest rules on test files
+  //   files: ['test/**'],
+  //   extends: [jestPlugin.configs['flat/recommended']],
+  // },
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+);
