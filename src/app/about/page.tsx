d04c012760855a1fe92ff3b10f9a5b7f6c6d5d22
Sergey Makoveev <sergey@makoveev.info>
2025-10-31 12:51:44 +0300

    page about: update title

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'about',
+  description: 'about page',
+};
+
 const Index = () => <h1>About [SSG]</h1>;
 
 export default Index;
