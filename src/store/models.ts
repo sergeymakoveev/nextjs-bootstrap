118a6041f429b9f9f52ba1dd7947708233160fa0
Sergey Makoveev <sergey@makoveev.info>
2025-11-02 23:51:44 +0300

    db: add post.likeCount

--- src/store/models.ts
+++ src/store/models.ts
@@ -1,14 +1,36 @@
-type DateSting = `${number}-${number}-${number}`;
+type DigitString = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+type YearString = `${'19' | '20'}${DigitString}${DigitString}`;
+
+// type MinutesString = `${'0' | '1' | '2' | '3' | '4' | '5'}${DigitString}`;
+// type SecondsString = MinutesString;
+// prettier-ignore
+// type HourString = '00' | '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23';
+
+// prettier-ignore
+type DayString = '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12' | '13' |'14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' | '31';
+// prettier-ignore
+type MonthString = '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12';
+type DateSting = `${YearString}-${MonthString}-${DayString}`;
+type DateTimeString = `${DateSting}T${number}:${number}:${number}Z`;
 
 export type Post = {
   id: number;
   title: string;
   content: string;
-  author: string;
+  authorId: number;
   date: DateSting;
+  likeCount?: number;
 };
 
 export type Author = {
   id: number;
   name: string;
+};
+
+export type Comment = {
+  id: number;
+  postId: number;
+  authorId: number;
+  content: string;
+  date: DateTimeString;
 };
