c426f332b872e1d27b6265bc06c0cce346ab7395
Sergey Makoveev <sergey@makoveev.info>
2025-11-12 09:31:41 +0300

    backend: add 1000 ms delay for browser responses

--- backend/server.middleware.js
+++ backend/server.middleware.js
@@ -1,7 +1,10 @@
 module.exports = (request, response, next) => {
   const isBrowserRequest = request.headers['user-agent'] !== 'node';
   if (isBrowserRequest) {
-    console.log('browser');
+    console.log('browser: set 1000 ms delay');
+    // Задержка браузерных запросов
+    setTimeout(next, 1000);
+  } else {
+    next();
   }
-  next();
 };
